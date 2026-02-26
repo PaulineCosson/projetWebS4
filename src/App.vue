b9370857f3efb410ef31dff73c4248532a35ae85
PaulineCosson <cosson.pa@gmail.com>
2026-02-26 16:15:32 +0100

    Initiation VueJS dogCenter

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <h1>Dog Center</h1>
+    <NewsSlider/>
+    <div id="dog-gallery">
+      <DogCard
+        v-for="dog in dogsData"
+        :key="dog.id"
+        :name="dog.name"
+        :age="dog.age"
+        :breed="dog.breed"
+        :pictureUrl="dog.pictureUrl"
+        :soundUrl="dog.soundUrl"
+      />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import dogsData from './dogsData'
+import DogCard from './components/DogCard.vue'
+import NewsSlider from './components/NewsSlider.vue'
+</script>
+
+<style scoped>
+#dog-gallery {
+  display: flex;
+  flex: wrap;
+}
+</style>
---
+++ src/dogsData.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    id: 1,
+    name: "Buddy",
+    age: 3,
+    breed: "Golden Retriever",
+    pictureUrl: "https://dog-center.com/api/dogs/1.png",
+    soundUrl: "https://dog-center.com/api/dogs/1.mp3"
+  },
+];
